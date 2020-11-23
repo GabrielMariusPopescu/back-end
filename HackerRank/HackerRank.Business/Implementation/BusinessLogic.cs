ef958a176cad46228da670a9607718cc2c1229ae
Gabriel Marius Popescu <GabyMarius@hotmail.com>
2020-11-23 07:37:45 +0000

    Sparse Array completed

--- HackerRank/HackerRank.Business/Implementation/BusinessLogic.cs
+++ HackerRank/HackerRank.Business/Implementation/BusinessLogic.cs
@@ -11,7 +11,8 @@ namespace HackerRank.Business.Implementation
             new List<string>
             {
                 "Exit",
-                "Left Rotation"
+                "Left Rotation",
+                "Sparse Array"
             };
 
         public Tuple<string, object, object> Run(int option)
@@ -23,6 +24,12 @@ namespace HackerRank.Business.Implementation
                     var initialList = new List<int> { 1, 2, 3, 4, 5 };
                     var resultList = leftRotation.Rotate(initialList, 2);
                     return new Tuple<string, object, object>(Challenges[option], initialList, resultList);
+                case 2:
+                    ISparseArray sparseArray = new SparseArray();
+                    var strings = new List<string> { "aba", "baba", "aba", "xzxb" };
+                    var queries = new List<string> { "aba", "xzxb", "ab" };
+                    var result = sparseArray.Sparse(strings, queries);
+                    return new Tuple<string, object, object>(Challenges[option], strings, result);
                 default:
                     return new Tuple<string, object, object>("", "", "");
             }
